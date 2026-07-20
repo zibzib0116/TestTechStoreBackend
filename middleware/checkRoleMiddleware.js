a896c78803b35fc9e9ffd176d047dc990b84fe23
zzzzzzzzzzzzzzzzzzzz <cccc@bai.vhj>
2026-07-20 22:51:38 +0400

    BackendDevelopment

---
+++ middleware/checkRoleMiddleware.js
@@ -0,0 +1,28 @@
+const jwt = require("jsonwebtoken");
+module.exports = function (role){
+    return  function(req, res, next){
+        if(req.method === 'OPTIONS'){
+            next()
+        }
+        try {
+            const token = req.headers.authorization.split(' ')[1];
+            if(!token){
+                return res.status(401).send('No token provided');
+            }
+            const decoded = jwt.verify(token, process.env.SECRET_KEY)
+            if(decoded.role!==role){
+                return res.status(403).send('No token provided');
+
+            }
+            req.user = decoded
+            next()
+
+        } catch(err){
+            res.status(401).json({message:"Unauthorized"})
+        }
+    }
+}
+
+
+
+
