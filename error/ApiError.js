ecda7cc6997a3dca4875c2a8acde06ce9e16aa69
zzzzzzzzzzzzzzzzzzzz <cccc@bai.vhj>
2026-08-03 17:30:27 +0400

    fix_code_erorr

--- error/ApiError.js
+++ error/ApiError.js
@@ -6,7 +6,7 @@ class ApiError extends Error {
     }
 
     static badRequest(message) {
-        return new ApiError(403,message);
+        return new ApiError(400,message);
     }
     static internal(message) {
         return new ApiError(500, message);
